d7c2eea67622b88ee66646c0a72db444185bd6e6
anarribes <anaa.ribeiro@hotmail.com>
2017-01-03 23:54:00 -0200

    saae

---
+++ src/pages/tratamento/tratamento.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Tratamento page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-tratamento',
+  templateUrl: 'tratamento.html'
+})
+export class Tratamento {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Tratamento Page');
+  }
+
+}
