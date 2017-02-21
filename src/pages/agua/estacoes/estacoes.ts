462dbcc57bb47bd7ac19756ce94c0a8c6a22ebd3
anarribes <anaa.ribeiro@hotmail.com>
2017-02-21 19:29:28 -0300

    Estacoes

--- src/pages/agua/estacoes/estacoes.ts
+++ src/pages/agua/estacoes/estacoes.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 /*
-  Generated class for the TesteCep page.
+  Generated class for the Estacoes page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-teste-cep',
-  templateUrl: 'teste-cep.html'
+  selector: 'page-estacoes',
+  templateUrl: 'estacoes.html'
 })
-export class TesteCep {
+export class Estacoes {
 
   constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
-    console.log('Hello TesteCep Page');
+    console.log('Hello Estacoes Page');
   }
 
 }
