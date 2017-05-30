821ac1a8a54f3f25bf91c207b2d9b2fda7591e48
anarribes <anaa.ribeiro@hotmail.com>
2017-05-30 18:53:24 -0300

    quase la 2

---
+++ src/pages/nossa-agua/saae/saae.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+//import { DataSaae } from '../../../components/data-saae/data-saae';
+
+import { SaaeHistoria } from 'saae-historia/saae-historia';
+
+
+@Component({
+  selector: 'page-saae',
+  templateUrl: 'saae.html'
+})
+  export class SaaePage {
+    //items = [];
+  //public dataList: DataSaae[];
+
+    constructor(public navCtrl: NavController) {
+      /*this.dataList = [
+          new DataSaae('Saae', [{title: 'História', component: SaaeHistoria, icon: 'ios-albums-outline'}
+                              ],'add-circle', false)
+      ];*/
+    }
+/*
+    openPageDiretores(diretores){
+        this.navCtrl.push(SaaeHistoria, {
+          pDiretores: diretores
+        });
+    }*/
+
+  }
